1bb4a527b536c92d94e8ca0d6160a21ee9a09517
prasoonm4774-collab <prasoonm4774@gmail.com>
2025-12-22 17:18:06 +0530

    Update RCA_4bit.v

--- RCA_4bit.v
+++ RCA_4bit.v
@@ -1,1 +1,12 @@
+module RCA_4bit(a,b,c,s);
+input [3:0] a,b;
+input c;
+output [4:0]s;
+wire c0,c1,c2;
+FA FA0(a[0],b[0],c,s[0],c0);
+FA FA1(a[1],b[1],c0,s[1],c1);
+FA FA2(a[2],b[2],c1,s[2],c2);
+FA FA3(a[3],b[3],c2,s[3],s[4]);
+endmodule
+
 
